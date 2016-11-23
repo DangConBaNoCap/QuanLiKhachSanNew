338cbe51f3da741ad5c6498b3dd143e347a04439
hakentty <hatrang070711@gmail.com>
2016-11-23 14:11:03 +0700

    update

--- QuanLiKhachSan/QuanLiKhachSan/FrmThemDichVu.Designer.cs
+++ QuanLiKhachSan/QuanLiKhachSan/FrmThemDichVu.Designer.cs
@@ -120,7 +120,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 231);
+            this.ClientSize = new System.Drawing.Size(323, 233);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.lbKT);
             this.Controls.Add(this.btnXacNhan);
